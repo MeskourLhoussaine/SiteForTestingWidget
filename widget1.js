9ca9bee5287832e9722e306b9743bcffb716df74
MeskourLhoussaine <127257257+MeskourLhoussaine@users.noreply.github.com>
2024-07-08 21:24:22 +0100

    update

--- widget1.js
+++ widget1.js
@@ -22,9 +22,8 @@ document.addEventListener("DOMContentLoaded", function() {
     var container = document.getElementById("content-behind-iframe");
     container.appendChild(button);
     
- ////////////////////////////////////////////////////////////////////////////////////////////////////////////////        
-// Click event listener for the button
-button.addEventListener("click", function() {
+    // Click event listener for the button
+    button.addEventListener("click", function() {
 
         // 
         var scriptElement = document.getElementById('paypik');
@@ -47,32 +46,37 @@ button.addEventListener("click", function() {
         // Log HMAC value
         console.log("HMAC: " + hmac);
 
-//////////////////////////////// Test host and access key and hmac
+        // Test host and access key and hmac
+        console.log('Response Permission:', currentURL);
+        console.log('Response Permission:', accessKey);
+        console.log('Response Permission:', marchandId);
+        console.log('Response Permission:', orderId);
+        console.log('Response Permission:', amount);
+        console.log('Response Permission:', currency);
+        console.log('Response Permission:', hmac);
 
-    var url = `http://localhost:8080/api/merchants/permission?hostname=${currentURL}&accessKey=${accessKey}&merchantId=${marchandId}&orderId=${orderId}&amount=${amount}&currency=${currency}&hmac=${hmac}`;
+        var url = `http://localhost:8080/api/merchants/permission?hostname=${currentURL}&accessKey=${accessKey}&merchantId=${marchandId}&orderId=${orderId}&amount=${amount}&currency=${currency}&hmac=${hmac}`;
 
-    // Fetch data from localhost
-    fetch(url)
-    .then(response => {
-        if (!response.ok) {
-        loadError();
-        throw new Error('Network response was not ok');
-        }
-        return response.json();
-    })
-    .then(data => {
-        console.log('Response Permission:', data);
-        if (data === true && data.status !== 400) {
-        loadWidget(accessKey, currentURL, marchandId, orderId, orderDescription, productsIds, amount, currency, hmac, redirectUrl);
-        } else {
-        loadError(); // Redirect to error page
-        }
-    })
-    .catch(error => console.error('Error fetching data from localhost:', error));
+        // Fetch data from localhost
+        fetch(url)
+        .then(response => {
+            if (!response.ok) {
+                loadError();
+                throw new Error('Network response was not ok');
+            }
+            return response.json(); // Return the JSON data from the response
+        })
+        .then(data => {
+            console.log('Response Permission:', data);
+            if (data === true && data.status !== 400) {
+                loadWidget(accessKey, currentURL, marchandId, orderId, orderDescription, productsIds, amount, currency, hmac, redirectUrl);
+            } else {
+                loadError(); // Redirect to error page
+            }
+        })
+        .catch(error => console.error('Error fetching data from localhost:', error));
 
-});
-
-////////////////////////////////
+    });
 
     function loadError() {
         var iframe = document.getElementById("myiframe");
@@ -88,8 +92,6 @@ button.addEventListener("click", function() {
         }
     }
 
-    //
-
     function loadWidget(accessKey, currentURL, marchandId, orderId, orderDescription, productsIds, amount, currency, hmac, redirectUrl) {
         var iframe = document.getElementById("myiframe");
         if (!iframe) {
@@ -103,9 +105,6 @@ button.addEventListener("click", function() {
             iframe.style.display = "block";
         }
     }
-    
-///////////////////////////////////////////////////////////////////////////////////////////////////////////////////
-
 
     // Event listener to handle message received from the widget
     window.addEventListener("message", function(event) {
@@ -135,81 +134,3 @@ button.addEventListener("click", function() {
     document.head.appendChild(styleElement);
 
 });
-
-
-//  document.addEventListener("DOMContentLoaded", function() {
-
-// /////////////////////////////////////////////////////////////////////////////////////////////////////////
-//     // Create the button
-//     var button = document.createElement("button");
-//     button.textContent = "Payer avec aman pay";
-//     button.id = "toggleButton";
-
-//     // Append the button to the desired container
-//     var container = document.getElementById("content-behind-iframe");
-//     container.appendChild(button);
-
-//     // Click 
-//     button.addEventListener("click", function() {
-//         var iframe = document.getElementById("myiframe");
-//         if (!iframe) {
-//             iframe = document.createElement("iframe");
-//             iframe.id = "myiframe";
-//             iframe.src = "https://anasanasri.github.io/autowidget/";
-//             iframe.style.width = "100%";
-//             iframe.style.height = "100%";
-//             container.appendChild(iframe);
-//         } else {
-//             iframe.style.display = "block";
-//         }
-//     });
-
-//     //
-//     window.addEventListener("message", function(event) {
-//         if (event.data === "closeWidget") {
-//             document.getElementById("myiframe").style.display = "none";
-//         }
-//     });
-    
-//     // Dynamically inject CSS styles
-//     var styles = `
-//         #toggleButton {
-//             background-color: green;
-//             color: white;
-//             border: round 1px solid;
-//             padding: 10px 20px;
-//             cursor: pointer;
-//             /* Add any other styles you desire */
-//         }
-//     `;
-
-//     var styleElement = document.createElement("style");
-//     styleElement.textContent = styles;
-//     document.head.appendChild(styleElement);
-// //////////////////////////////////////////////////////////////////////////////////////////////////////////////
-// // Get reference to the script element
-// var scriptElement = document.getElementById('paypik');
-
-// // Get the values of data-access_key and data-host attributes
-// var accessKey = scriptElement.getAttribute('data-access_key');
-// var host = scriptElement.getAttribute('data-host');
-
-// // Log the data
-// console.log("Data to be sent to Angular project:", { accessKey, host });
-
-// // Construct the URL with query parameters
-// var url = 'https://anasanasri.github.io/autowidget/?accessKey=' + encodeURIComponent(accessKey) + '&host=' + encodeURIComponent(host);
-
-// // Using fetch to send data to Angular project using GET request
-// fetch(url)
-//   .then(function(response) {
-//     if (!response.ok) {
-//       throw new Error('Network response was not ok');
-//     }
-//     console.log('Data sent successfully to Angular project');
-//   })
-//   .catch(function(error) {
-//     console.error('Error sending data to Angular project:', error);
-//   });
-// });
-

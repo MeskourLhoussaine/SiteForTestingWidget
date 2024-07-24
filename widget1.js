7cbab53497ee08b820a49087314f2a101ad4d20e
MeskourLhoussaine <127257257+MeskourLhoussaine@users.noreply.github.com>
2024-07-24 09:24:59 +0100

    update

--- widget1.js
+++ widget1.js
@@ -1,5 +1,4 @@
 document.addEventListener("DOMContentLoaded", function() {
-    
     // Création du bouton
     var button = document.createElement("button");
     button.id = "toggleButton";
@@ -21,10 +20,9 @@ document.addEventListener("DOMContentLoaded", function() {
     // Ajouter le bouton au conteneur désiré
     var container = document.getElementById("content-behind-iframe");
     container.appendChild(button);
-    
+
     // Écouteur d'événements de clic pour le bouton
     button.addEventListener("click", function() {
-
         // Récupération de l'élément de script
         var scriptElement = document.getElementById('paypik');
 
@@ -38,11 +36,11 @@ document.addEventListener("DOMContentLoaded", function() {
         var currency = scriptElement.getAttribute('data-currency');
         var hmac = scriptElement.getAttribute('data-hmac');
         var redirectUrl = scriptElement.getAttribute('data-redirect_url');
-        
+
         // Récupération de l'URL actuelle
-        var currentURL = window.location.href;    
+        var currentURL = window.location.href;
         console.log(currentURL);
-        
+
         // Affichage de la valeur HMAC dans la console
         console.log("HMAC: " + hmac);
 
@@ -51,25 +49,23 @@ document.addEventListener("DOMContentLoaded", function() {
 
         // Effectuer la requête fetch vers le serveur local
         fetch(url)
-        .then(response => {
-            if (!response.ok) {
-                loadError();
-                throw new Error('Réponse du réseau non valide');
-            }
-            return response.json(); // Retourner les données JSON de la réponse
-        })
-        .then(data => {
-            console.log('Réponse Permission:', data);
-            if (data === true && data.status !== 400) {
-                loadWidget(accessKey, currentURL, marchandId, orderId, orderDescription, productsIds, amount, currency, hmac, redirectUrl);
-            } else {
-                loadError(); // Redirection vers la page d'erreur en cas de problème
-            }
-        })
-        .catch(error => console.error('Erreur lors de la récupération des données depuis le serveur local:', error));
-
+            .then(response => {
+                if (!response.ok) {
+                    loadError();
+                    throw new Error('Réponse du réseau non valide');
+                }
+                return response.json(); // Retourner les données JSON de la réponse
+            })
+            .then(data => {
+                console.log('Réponse Permission:', data);
+                if (data === true && data.status !== 400) {
+                    loadWidget(accessKey, currentURL, marchandId, orderId, orderDescription, productsIds, amount, currency, hmac, redirectUrl);
+                } else {
+                    loadError(); // Redirection vers la page d'erreur en cas de problème
+                }
+            })
+            .catch(error => console.error('Erreur lors de la récupération des données depuis le serveur local:', error));
     });
-    
 
     // Fonction pour charger la page d'erreur
     function loadError() {
@@ -128,4 +124,53 @@ document.addEventListener("DOMContentLoaded", function() {
     styleElement.textContent = styles;
     document.head.appendChild(styleElement);
 
+    // Initialiser le panier
+    const cart = [];
+    const cartSummary = document.querySelector('.cart-summary'); // Assurez-vous que la classe est correctement assignée
+
+    // Fonction pour mettre à jour le résumé du panier
+    function updateCartSummary() {
+        let totalAmount = 0;
+        cartSummary.innerHTML = ''; // Réinitialiser le résumé du panier
+
+        cart.forEach(item => {
+            totalAmount += item.price;
+            cartSummary.innerHTML += `
+                <li class="grid grid-cols-6 gap-2 border-b-1">
+                    <div class="col-span-1 self-center">
+                        <img src="${item.image}" alt="Produit" class="rounded w-full">
+                    </div>
+                    <div class="flex flex-col col-span-3 pt-2">
+                        <span class="text-gray-600 text-md font-semi-bold">${item.name}</span>
+                        <span class="text-gray-400 text-sm inline-block pt-2">Produit</span>
+                    </div>
+                    <div class="col-span-2 pt-3">
+                        <div class="flex items-center space-x-2 text-sm justify-between">
+                            <span class="text-gray-400">1 x ${item.price.toFixed(2)} DH</span>
+                            <span class="text-emerald-400 font-semibold inline-block">${item.price.toFixed(2)} DH</span>
+                        </div>
+                    </div>
+                </li>
+            `;
+        });
+
+        // Afficher le total
+        document.querySelector('.cart-total').textContent = totalAmount.toFixed(2);
+    }
+
+    // Événement pour ajouter au panier
+    document.querySelectorAll('.add-to-cart').forEach(button => {
+        button.addEventListener('click', function () {
+            const id = this.getAttribute('data-id');
+            const name = this.getAttribute('data-name');
+            const image = this.getAttribute('src');
+            const price = parseFloat(this.getAttribute('data-price'));
+
+            // Ajouter l'article au panier
+            cart.push({ id, name, image, price });
+
+            // Mettre à jour le résumé du panier
+            updateCartSummary();
+        });
+    });
 });
